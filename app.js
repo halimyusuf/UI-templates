1e8f51f60101ae88b41d797d857064bd9d9cc8fc
halimyusuf <35901910+halimyusuf@users.noreply.github.com>
2019-01-19 23:26:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+require('./server/routes/routes')(app);
+require('./server/models/prod')(app);
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => { console.log(`Listening on port ${port}...`); });
+
+module.exports = server;
+
