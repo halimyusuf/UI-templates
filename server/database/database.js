135e1ef8f603857d2766426487a54e4ad3b2e35b
halimyusuf <35901910+halimyusuf@users.noreply.github.com>
2019-02-02 00:30:55 +0100

    Update database.js

--- server/database/database.js
+++ server/database/database.js
@@ -1,30 +1,30 @@
-const { Pool }  = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const pool = new Pool({
-  connectionString: process.env.DB_URL
-});
-
-module.exports = {
-  /**
-   * DB Query
-   * @param {object} req
-   * @param {object} res
-   * @returns {object} object 
-   */
-  query(text, params){
-    return new Promise((resolve, reject) => {
-      pool.query(text, params)
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        reject(err);
-      })
-    })
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt-nodejs')
+const config = require('config')
+const Helper = {
+    
+    hashPassword(password) {
+      return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+    },
+    
+    comparePassword(hashPassword, password) {
+      return bcrypt.compareSync(password, hashPassword);
+    },
+    
+    isValidEmail(email) {
+      return /\S+@\S+\.\S+/.test(email);
+    },
+    
+    generateToken(id,user) {
+      const token = jwt.sign({
+        userId: id,
+        username: user
+      },
+        config.get('jwtPrivateKey'), { expiresIn: '7d' }
+      );
+      return token;
+    }
   }
-}
-
-// DB_URL = postgres://postgres@127.0.0.1:5432/test
+  
+  module.exports = Helper
+  require('make-runnable')
